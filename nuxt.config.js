ca5307392c162945d1db3b2139ac2b93bf737f53
anton <>
2021-03-20 22:25:03 +0200

    npm - remove @nuxt/google-analytics, add vue-tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,15 +22,18 @@ export default {
 	],
 
 	// Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-	plugins: [],
+	plugins: [
+        {
+            src: '@/plugins/gtag.js',
+            mode: 'client'
+        }
+    ],
 
 	// Auto import components (https://go.nuxtjs.dev/config-components)
 	components: true,
 
 	// Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-	buildModules: [
-        '@nuxtjs/google-analytics'
-    ],
+	buildModules: [],
 
 	// Modules (https://go.nuxtjs.dev/config-modules)
 	modules: [],
---
+++ plugins/gtag.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import VueGtag from 'vue-gtag';
+
+Vue.use(VueGtag, {
+    config: {
+        id: 'G-4P6HNKCS39'
+    },
+    appName: 'pscovers.shop',
+});
